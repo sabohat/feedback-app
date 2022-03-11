cf1438c6e6887935f4d72f7590c9fb9ea91121e5
sabohat <sabohat2802@gmail.com>
2022-03-11 15:55:46 +0500

    add responsive footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <div className="wrapper flex flex-col items-center justify-center space-y-2 text-gray-500 text-sm">
-        <span className=" underline">
+      <div className="wrapper flex flex-col items-center justify-center space-y-3  text-gray-500 text-sm text-center">
+        <span className="underline">
           Westminster International University in Tashkent
         </span>
         <span className="">Upgrade Contest 2022</span>
