3b6d6ed68ec6ddaacae0beb38256b674cbc1b888
sabohat <sabohat2802@gmail.com>
2022-03-11 14:50:11 +0500

    update footer text color

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <div className="wrapper flex flex-col items-center justify-center space-y-2">
+      <div className="wrapper flex flex-col items-center justify-center space-y-2 text-gray-500 text-sm">
         <span className=" underline">
           Westminster International University in Tashkent
         </span>
