ee544f2aa9d151e1f83d3f925be65b035b4a0c52
sabohat <sabohat2802@gmail.com>
2022-03-11 13:34:16 +0500

    feat: add Tailwind jit feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blackFont: "#363F54",
+        primaryGreen: "#08C3A8",
+        primaryBlue: "#4BADEF",
+      },
+    },
   },
   plugins: [],
 };
